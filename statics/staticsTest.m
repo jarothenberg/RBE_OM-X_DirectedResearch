8d5d5e4d7881d92a3f7099c1054ad51500ff9c14
Jack Rothenberg <jarothenberg@wpi.edu>
2023-04-12 19:55:48 -0400

    Added code to find the change in torque given the change in force. Needs work in terms of possible conversions and other.

--- statics/staticsTest.m
+++ statics/staticsTest.m
@@ -4,11 +4,33 @@ clc
 robot = Robot();
 robot.writeMotorState(true);
 robot.setOperatingMode('p');
-robot.gripper.toggleTorque(false);
 travTime = 5;
 robot.writeTime(travTime);
 robot.writeJoints(0);
 pause(travTime);
+
+while true
+    
+    read = robot.getJointsReadings();
+    currents = (read(3,:)/1000)';
+    t_0 = (currents-0.18).*((3.01-0.07)/(1.8-0.18))+0.07
+    
+    pause(5)
+    robot.writeGripper(true);
+    pause(5)
+    robot.writeGripper(false);
+    pause(5)
+    read = robot.getJointsReadings();
+    currents = (read(3,:)/1000)';
+    t_1 = (currents-0.18).*((3.01-0.07)/(1.8-0.18))+0.07
+    
+    dt_m = t_1 - t_0
+    %is the downward direction in the J y?
+    df = [0 0 0 0 0 0]'; %payload
+    %is jacobian in mm? if so maybe some conversion needs to happen?
+    J = robot.getJacobian(read(1,:));
+    dt_c = J'*df
+end
 
 % tic
 while true
@@ -20,7 +42,7 @@ while true
 
     % https://ocw.mit.edu/courses/2-12-introduction-to-robotics-fall-2005/5c160fb678fa75191c399a373c6ce648_chapter6.pdf
     force = inv(J(1:3,1:3)') * torque(1:3)';
-    weight = force(3)/4.448
+    weight = force(3)/4.448;
 
 end
 robot.writeVelocities([0 0 0 0]);
